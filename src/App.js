3a3db2b93319e905057cb20137dbbddc5d767baf
tuanhoangle <tuanleprofessional@gmail.com>
2022-01-12 16:36:39 -0500

    navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+import Navbar from "./components/Navbar";
+
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Switch>
         <Route exact path="/ewce" component={Login} />
         <Route exact path="/home" component={Home} />
